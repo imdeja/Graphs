4d5c676b3698299e32172574f948f434ac9116b1
imdeja <jakedennis877@gmail.com>
2020-09-11 08:58:55 -0700

    mvp SC: 1004 moves, 500 rooms visited

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -28,7 +28,30 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+visited = {}
+reversed_traversal_path = []
+directions = {"n": "s", "e": "w", "s": "n", "w": "e"}
 
+visited[player.current_room.id] = player.current_room.get_exits()
+
+while len(visited) < len(room_graph):
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = player.current_room.get_exits()
+        previous_room = reversed_traversal_path[-1]
+        visited[player.current_room.id].remove(previous_room)
+
+    if len(visited[player.current_room.id]) == 0:
+        previous_room = reversed_traversal_path[-1]
+        reversed_traversal_path.pop()
+        traversal_path.append(previous_room)
+        player.travel(previous_room)
+
+    else:
+        visit = visited[player.current_room.id][-1]
+        visited[player.current_room.id].pop()
+        traversal_path.append(visit)
+        reversed_traversal_path.append(directions[visit])
+        player.travel(visit)
 
 
 # TRAVERSAL TEST
